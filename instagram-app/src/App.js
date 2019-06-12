e44db286be2b64c884c6bb66c730df9c272cf0c8
anthonyvigz <anthonyvigz@gmail.com>
2019-06-11 21:56:16 -0400

    imported dummy data into the state

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import dummyData from './dummy-data';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      data: dummyData
     }
   }
   
