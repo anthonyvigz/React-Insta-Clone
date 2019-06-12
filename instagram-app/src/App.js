af5346d15c669009fbbf927d0777e1a282fc8803
anthonyvigz <anthonyvigz@gmail.com>
2019-06-11 22:00:05 -0400

    created post container

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends React.Component {
   constructor() {
@@ -14,7 +15,7 @@ class App extends React.Component {
   render() {
     return (
     <div className="App">
-        Howdy
+        <PostContainer data={this.state.data} />
     </div>
     )
   }
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostContainer = props => {
+    return (
+        <div>hey</div>
+    )
+}
+
+export default PostContainer;
