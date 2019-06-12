221ccef9a5ab56988df320d87040edf8947c64f8
anthonyvigz <anthonyvigz@gmail.com>
2019-06-11 21:53:35 -0400

    setting up app workspace

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  
+  
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Howdy
     </div>
-  );
+    )
+  }
 }
 
 export default App;
